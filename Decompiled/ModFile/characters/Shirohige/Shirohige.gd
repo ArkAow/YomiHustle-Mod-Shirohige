60d59e29305039a3968c3ee97af8981acc9f3306
ArkAow <tom.charpentier@outlook.fr>
2023-10-03 14:50:31 +0100

    changement fichiers sprites

--- Decompiled/ModFile/characters/Shirohige/Shirohige.gd
+++ Decompiled/ModFile/characters/Shirohige/Shirohige.gd
@@ -12,37 +12,25 @@ var hakiAvailableStance = "HakiAvailable"
 
 func tick():
 	.tick()
-	print(currentHakiAmount)
 	if (currentHakiAmount > 0):
 		hakiEmpty = false
 		if (currentHakiAmount >= maximumHakiAmount):
 			hakiFull = true
 			change_stance_to(hakiAvailableStance)
-			print(stance)
 		else:
 			hakiFull = false
 	else:
 		if(currentHakiAmount <= 0):
 			hakiEmpty = true
 	
-	if(hakiFull):
-		print("Haki plein")
-		
-	if(hakiEmpty):
-		print("Haki vide")
-	
 	if (hakiActivated):
 		if (hakiFull):
 			change_stance_to(hakiStance)
-			print(stance)
 			hakiRegenerate = false
-			print("Haki activé")
 		else:
 			if (hakiEmpty):
 				change_stance_to(normalStance)
-				print(stance)
 				hakiActivated = false
-				print("Haki désactivé")
 	else:
 		if (!hakiFull):
 			hakiRegenerate = true
